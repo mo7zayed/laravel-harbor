2bc53c378a6096fb6656a1773f618f3a32a80700
Mohamed Zayed <mohamedzayed907@gmail.com>
2025-09-09 15:40:59 +0300

    Improve error handling by adding context to SSL certification failure messages in ObtainLetsEncryptCertification

--- app/Services/Forge/Pipeline/ObtainLetsEncryptCertification.php
+++ app/Services/Forge/Pipeline/ObtainLetsEncryptCertification.php
@@ -39,7 +39,7 @@ class ObtainLetsEncryptCertification
             );
         } catch (Throwable $e) {
             $this->failCommand("---> Something's wrong with SSL certification. Check your Forge site Log for more info.");
-            $this->failCommand($e->getMessage());
+            $this->failCommand("---> " . $e->getMessage());
         }
 
         return $next($service);
